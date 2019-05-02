3bb67cb54c71881341040d312c2189bccb11e452
Casey Ross <caseyross003@gmail.com>
2019-05-02 16:21:41 -0500

    add ar scene with barcode marker

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import 'aframe';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="SideBar"></div>
-      <div className="Scene">
-        <a-scene>
-          <a-entity 
-            geometry="primitive: box" 
-            position="-1 0.5 -3" 
-            rotation="0 45 0" 
-            material="color: #4CC3D9" />
-          <a-entity 
-            geometry="primitive: sphere; radius: 1.25;" 
-            position="0 1.25 -5" 
-            material="color: #EF2D5E" />
-          <a-entity 
-            geometry="primitive: cylinder; radius: 0.5, height: 1.5" 
-            position="1 0.75 -3" 
-            material="color: #FFC65D" />
-          <a-entity 
-            geometry="primitive: plane; width: 4; height: 4" 
-            position="0 0 -4" 
-            rotation="-90 0 0" 
-            material="color: #7BC8A4" />
-          <a-sky src={require('./assets/img/big_sky.png')} />
-        </a-scene>
-      </div>
+      <a-scene embedded arjs='detectionMode: mono_and_matrix; matrixCodeType: 3x3;'>
+
+        <a-marker type='barcode' value='5'>
+          <a-box position='0 0.5 0' material='color: blue;'></a-box>
+        </a-marker>
+
+        <a-entity camera></a-entity>
+      </a-scene>
     </div>
   );
 }
