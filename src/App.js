31e285d5a585e5b9cc0c6a95e7c6959af9752f15
Casey Ross <caseyross003@gmail.com>
2019-05-02 10:48:36 -0500

    add aframe package and background to scene

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'aframe';
 import './App.css';
 
 function App() {
@@ -7,11 +8,25 @@ function App() {
       <div className="SideBar"></div>
       <div className="Scene">
         <a-scene>
-          <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-          <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-          <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-          <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-          <a-sky color="#ECECEC"></a-sky>
+          <a-entity 
+            geometry="primitive: box" 
+            position="-1 0.5 -3" 
+            rotation="0 45 0" 
+            material="color: #4CC3D9" />
+          <a-entity 
+            geometry="primitive: sphere; radius: 1.25;" 
+            position="0 1.25 -5" 
+            material="color: #EF2D5E" />
+          <a-entity 
+            geometry="primitive: cylinder; radius: 0.5, height: 1.5" 
+            position="1 0.75 -3" 
+            material="color: #FFC65D" />
+          <a-entity 
+            geometry="primitive: plane; width: 4; height: 4" 
+            position="0 0 -4" 
+            rotation="-90 0 0" 
+            material="color: #7BC8A4" />
+          <a-sky src={require('./assets/img/big_sky.png')} />
         </a-scene>
       </div>
     </div>
@@ -19,3 +34,4 @@ function App() {
 }
 
 export default App;
+
